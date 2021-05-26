bc8e36fe3b5340db4675361e03958fff40df35b0
Cindy <cg6671510@gmail.com>
2021-05-26 16:25:48 -0500

    added occurence

--- SampleFolder/coding.js
+++ SampleFolder/coding.js
@@ -10,3 +10,19 @@ function binary(decimal) {
  return result.split("").reverse().join("");
 }
 }
+//Create a function given a string find a letter that has a single occurence. Return the letter
+// in uppercase. If input is empty, return an empty string
+
+function singleOccurence(str) {
+    str = str.UpperCase();
+    if(str === ""){
+        return str;
+    }
+    for (i =0; i <str.length; i++) {
+        if (str.indexOf(str.chartAt(i))== str.lastIndexOf(str.charAt(i))) {
+            return str.charAt(i);
+        }
+    }
+
+}
+
